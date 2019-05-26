5d8a3517f66350932ad3cbba1afff76f688d610f
Yusuke Inoue <yi@Yusuke-no-MacBook-Air.local>
2019-05-26 14:05:36 +0900

    add ogp

--- src/App.js
+++ src/App.js
@@ -10,6 +10,22 @@ import RouteAbout from './component/RouteAbout.js';
 const App = () => {
   return (
   <Container>
+    <Helmet
+      title={'Hello World'}
+      meta={[
+        { name: 'twitter:title', content: 'Y.Inoue' },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:image', content: 'https://defunty.dev/img/ogp.png' },
+        { name: 'twitter:description', content: "Yusuke Inoue's Portfolio. Web developer." },
+        { name: 'twitter:url', content: 'https://defunty.dev/' },
+        { name: 'twitter:site', content: '@defunty_jp' },
+        { name: 'twitter:domain', content: 'defunty.dev' },
+        { property: 'og:image', content: 'https://defunty.dev/img/ogp.png' },
+        { property: 'og:title', content: 'Y.Inoue' },
+        { property: 'og:description', content: "Yusuke Inoue's Portfolio. Web developer." },
+        { property: 'og:url', content: `Y.Inoue` }
+      ]}
+    />
     <BrowserRouter>
       <Route exact path='/' component={RouteTop} />
       <Route path="/about" component={RouteAbout} />
