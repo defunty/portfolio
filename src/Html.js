0ca3459148681cb0e596878aec9d46266a14cd77
Yusuke Inoue <yi@Yusuke-no-MacBook-Air.local>
2019-05-26 15:31:05 +0900

    mod

--- src/Html.js
+++ src/Html.js
@@ -7,7 +7,7 @@ const Html = (props) => {
         <title>Test</title>
       </head>
       <body>
-        <div id="app">{props.children}</div>
+        <div id="app">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa{props.children}</div>
         <script id="initial-data" type="text/plain" data-json={props.initialData}></script>
         <script src="/static/bundle.js"></script>
       </body>
